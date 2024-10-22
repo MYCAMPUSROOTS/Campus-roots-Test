16ae84d2b7f77c8b05885ab08e3b875bcf000a6f
Stevvenn <108819512+Stevvenn@users.noreply.github.com>
2024-10-22 20:02:00 +0100

    Init commit

--- app/src/main/java/com/example/campusrootsinternapp/HomeFragment.kt
+++ app/src/main/java/com/example/campusrootsinternapp/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.campusrootsinternapp.databinding.ActivityMainBinding
 import com.example.campusrootsinternapp.databinding.FragmentFragmentBinding
 
 class HomeFragment : Fragment() {
