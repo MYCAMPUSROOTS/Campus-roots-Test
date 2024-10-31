1b6b9b09f20780ea3c2ad8913106a49700820ea6
ProCrafters <73192264+1ProCrafters@users.noreply.github.com>
2024-10-31 13:06:44 -0500

    Per course fragment

--- app/src/main/java/com/example/campusrootsinternapp/Fragment1.kt
+++ app/src/main/java/com/example/campusrootsinternapp/Fragment1.kt
@@ -5,16 +5,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.campusrootsinternapp.FragmentCourse
 import com.example.campusrootsinternapp.R
 import com.example.campusrootsinternapp.adapter.Adapter
 import com.example.campusrootsinternapp.base.BaseFragment
+import com.example.campusrootsinternapp.databinding.FragmentDashboardBinding
 import com.example.campusrootsinternapp.model.CourseItem
+import com.example.campusrootsinternapp.util.observeChange
 
 class Fragment1 : BaseFragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: Adapter
     private lateinit var courseList: ArrayList<CourseItem>
+
+    private lateinit var courseContainer: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,24 +42,21 @@ class Fragment1 : BaseFragment() {
         courseList.add(CourseItem("MTH 101", "Student Elementary Mathematics TA", "Markus Callaway", 7, false))
         courseList.add(CourseItem("BUS 103", "Introduction to Business", "Markus Callaway", 7, false))
 
-        courseList.add(CourseItem("MTH 101", "Student Elementary Mathematics TA", "Markus Callaway", 7, true))
-        courseList.add(CourseItem("PHY 211", "Principles of Quantum Physics", "Markus Callaway", 7, true))
-        courseList.add(CourseItem("BUS 103", "Introduction to Business", "Markus Callaway", 7, false))
-        courseList.add(CourseItem("PHY 211", "Principles of Quantum Physics", "Markus Callaway", 7, false))
-        courseList.add(CourseItem("MTH 101", "Student Elementary Mathematics TA", "Markus Callaway", 7, false))
-        courseList.add(CourseItem("BUS 103", "Introduction to Business", "Markus Callaway", 7, false))
-
-        courseList.add(CourseItem("MTH 101", "Student Elementary Mathematics TA", "Markus Callaway", 7, true))
-        courseList.add(CourseItem("PHY 211", "Principles of Quantum Physics", "Markus Callaway", 7, true))
-        courseList.add(CourseItem("BUS 103", "Introduction to Business", "Markus Callaway", 7, false))
-        courseList.add(CourseItem("PHY 211", "Principles of Quantum Physics", "Markus Callaway", 7, false))
-        courseList.add(CourseItem("MTH 101", "Student Elementary Mathematics TA", "Markus Callaway", 7, false))
-        courseList.add(CourseItem("BUS 103", "Introduction to Business", "Markus Callaway", 7, false))
-
-
-        // Set up the adapter
+                // Set up the adapter
         adapter = Adapter(courseList)
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
+
+        adapter.selectedCourse.observeChange(viewLifecycleOwner) { selectedCourse ->
+            val fragment = FragmentCourse()
+            fragment.arguments = Bundle().apply {
+                putString("courseName", selectedCourse.courseName)
+                putString("courseTitle", selectedCourse.courseTitle)
+                putString("courseLecturer", selectedCourse.courseLecturer)
+                putInt("courseChannels", selectedCourse.courseChannels)
+            }
+            mFragmentNavigation.pushFragment(fragment)
+
+        }
     }
 }
